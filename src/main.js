092a2d77d4922794b1809e1cf8d20e59f4394481
iiishokoiii <tobby_36@yahoo.co.jp>
2020-04-21 16:47:48 +0900

    firebaseの導入

--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./scss/main.scss";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxLft0OOKPjJuht_Uo7J40EIT8kFYUJ7Q",
+  authDomain: "iii-app-todolist.firebaseapp.com",
+  databaseURL: "https://iii-app-todolist.firebaseio.com",
+  projectId: "iii-app-todolist",
+  storageBucket: "iii-app-todolist.appspot.com",
+  messagingSenderId: "588860085203",
+  appId: "1:588860085203:web:af59f56eb58dda72a55526"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
